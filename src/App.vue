16fc342e9220ebab3ae6064973a5fd519311fc4a
Dmitry Kutsenko <d2emonium@gmail.com>
2018-01-14 23:19:15 +0300

    Start page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,65 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <v-app id="inspire" dark>
+    <v-navigation-drawer
+      clipped
+      fixed
+      v-model="drawer"
+      app
+    >
+      <v-card>
+        <v-card-text>
+          <blockquote class="blockquote caption">
+            <p>Если неприятность может произойти, она обязательно происходит.</p>
+            <p class="grey--text">Закон Мерфи, 2-я пол. ХХ века</p>
+          </blockquote>
+        </v-card-text>
+      </v-card>
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>dashboard</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Dashboard</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Settings</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Вереница Миров</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+          <img src="./assets/logo.png">
+          <router-view/>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer app fixed>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
+import('vuetify/dist/vuetify.min.css')
+
 export default {
-  name: 'App'
+  name: 'App',
+  data: () => ({
+    drawer: null,
+    source: 'String'
+  })
 }
 </script>
 
