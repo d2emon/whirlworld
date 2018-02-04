25b032da538b672315afc3b0cf1bfa10771884d2
Dmitry Kutsenko <d2emonium@gmail.com>
2018-02-04 21:22:20 +0300

    Item icons

--- src/components/ItemIcon.vue
+++ src/components/ItemIcon.vue
@@ -3,13 +3,19 @@
     <v-card slot="activator">
       <v-card-media v-if="item.image" :src="'/static/items/' + item.image" height="100px">
       </v-card-media>
+    </v-card>
+    <v-card>
+      <v-card-title primary-title class="headline">
+        {{ item.title }}
+      </v-card-title>
+      <v-card-text>
+        <div v-if="item.description">{{ item.showDescription() }}</div>
+        <div v-if="item.full">{{ item.full }}</div>
+      </v-card-text>
       <v-card-actions>
         <v-btn flat dark>Listen now</v-btn>
       </v-card-actions>
     </v-card>
-    <h1>{{ item.title }}</h1>
-    <div v-if="item.description">{{ item.showDescription() }}</div>
-    <div v-if="item.full">{{ item.full }}</div>
   </v-tooltip>
 </template>
 
