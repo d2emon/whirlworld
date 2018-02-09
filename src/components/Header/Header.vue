c53c7ab4fa460aa852e4e6b6a261f6c4e9cdbdc1
Dmitry Kutsenko <d2emonium@gmail.com>
2018-02-09 22:30:40 +0300

    Translated header

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -8,148 +8,77 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <b-navbar-nav class="d-md-down-none">
-      <b-nav-item class="px-3">Dashboard</b-nav-item>
-      <b-nav-item class="px-3">Users</b-nav-item>
-      <b-nav-item class="px-3">Settings</b-nav-item>
+      <b-nav-item class="px-3" v-for="(item, id) in mainMenu" :key="id" :to="item.url">{{ item.title }}</b-nav-item>
     </b-navbar-nav>
     <b-navbar-nav class="ml-auto">
       <b-nav-item-dropdown right no-caret>
         <template slot="button-content">
           <i class="icon-bell"></i>
-          <b-badge pill variant="danger">5</b-badge>
+          <b-badge pill variant="danger" v-if="notifications.length > 0">{{ notifications.length }}</b-badge>
         </template>
-        <b-dropdown-header tag="div" class="text-center dropdown-menu-lg"><strong>You have 5 notifications</strong></b-dropdown-header>
-        <b-dropdown-item><i class="icon-user-follow text-success"></i> New user registered</b-dropdown-item>
-        <b-dropdown-item><i class="icon-user-unfollow text-danger"></i> User deleted</b-dropdown-item>
-        <b-dropdown-item><i class="icon-chart text-info"></i> Sales report is ready</b-dropdown-item>
-        <b-dropdown-item><i class="icon-basket-loaded text-primary"></i> New client</b-dropdown-item>
-        <b-dropdown-item><i class="icon-speedometer text-warning"></i> Server overloaded</b-dropdown-item>
-        <b-dropdown-header tag="div" class="text-center"><strong>Server</strong></b-dropdown-header>
+        <template v-if="notifications.length > 0">
+          <b-dropdown-header tag="div" class="text-center dropdown-menu-lg"><strong>У вас {{ notifications.length }} оповещений</strong></b-dropdown-header>
+          <b-dropdown-item v-for="(item, id) in notifications.slice(0, 5)" :key="id"><i :class="item.icon"></i> {{ item.text }}</b-dropdown-item>
+        </template>
+        <b-dropdown-header tag="div" class="text-center"><strong>Сервер</strong></b-dropdown-header>
         <b-dropdown-item>
-          <div class="text-uppercase mb-1"><small><b>CPU Usage</b></small></div>
-          <b-progress height={} class="progress-xs" variant="info" :value="25"></b-progress>
-          <small class="text-muted">348 Processes. 1/4 Cores.</small>
+          <div class="text-uppercase mb-1"><small><b>Использование CPU</b></small></div>
+          <b-progress height={} class="progress-xs" variant="info" :value="(server.cpu.value / server.cpu.max) * 100"></b-progress>
+          <small class="text-muted">{{ server.cpu.processes }} Процессов. {{ server.cpu.value }}/{{ server.cpu.max }} Ядер.</small>
         </b-dropdown-item>
         <b-dropdown-item>
-          <div class="text-uppercase mb-1"><small><b>Memory Usage</b></small></div>
-          <b-progress height={} class="progress-xs" variant="warning" :value="70"></b-progress>
-          <small class="text-muted">11444GB/16384MB</small>
+          <div class="text-uppercase mb-1"><small><b>Использование Памяти</b></small></div>
+          <b-progress height={} class="progress-xs" variant="warning" :value="(server.memory.value / server.memory.max) * 100"></b-progress>
+          <small class="text-muted">{{ server.memory.value }}MB/{{ server.memory.max }}MB</small>
         </b-dropdown-item>
         <b-dropdown-item>
-          <div class="text-uppercase mb-1"><small><b>SSD 1 Usage</b></small></div>
-          <b-progress height={} class="progress-xs" variant="danger" :value="95"></b-progress>
-          <small class="text-muted">243GB/256GB</small>
+          <div class="text-uppercase mb-1"><small><b>Использование SSD 1</b></small></div>
+          <b-progress height={} class="progress-xs" variant="danger" :value="(server.ssd.value / server.ssd.max) * 100"></b-progress>
+          <small class="text-muted">{{ server.ssd.value }}GB/{{ server.ssd.max }}GB</small>
         </b-dropdown-item>
       </b-nav-item-dropdown>
 
       <b-nav-item-dropdown right no-caret>
         <template slot="button-content">
           <i class="icon-list"></i>
-          <b-badge pill variant="warning">15</b-badge>
+          <b-badge pill variant="warning" v-if="tasks.length > 0">{{ tasks.length }}</b-badge>
         </template>
-        <b-dropdown-header tag="div" class="text-center dropdown-menu-lg"><strong>You have 15 pending tasks</strong></b-dropdown-header>
-        <b-dropdown-item>
-          <div class="small mb-1">Upgrade NPM &amp; Bower <span class="float-right"><strong>0%</strong></span></div>
-          <b-progress height={} class="progress-xs" variant="info" :value="0"></b-progress>
-        </b-dropdown-item>
-        <b-dropdown-item>
-          <div class="small mb-1">ReactJS Version <span class="float-right"><strong>25%</strong></span></div>
-          <b-progress height={} class="progress-xs" variant="danger" :value="25"></b-progress>
-        </b-dropdown-item>
-        <b-dropdown-item>
-          <div class="small mb-1">VueJS Version <span class="float-right"><strong>50%</strong></span></div>
-          <b-progress height={} class="progress-xs" variant="warning" :value="50"></b-progress>
-        </b-dropdown-item>
-        <b-dropdown-item>
-          <div class="small mb-1">Add new layouts <span class="float-right"><strong>75%</strong></span></div>
-          <b-progress height={} class="progress-xs" variant="info" :value="75"></b-progress>
-        </b-dropdown-item>
-        <b-dropdown-item>
-          <div class="small mb-1">Angular 2 Cli Version <span class="float-right"><strong>100%</strong></span></div>
-          <b-progress height={} class="progress-xs" variant="success" :value="100"></b-progress>
-        </b-dropdown-item>
-        <b-dropdown-item class="text-center"><strong>View all tasks</strong></b-dropdown-item>
+        <template v-if="tasks.length > 0">
+          <b-dropdown-header tag="div" class="text-center dropdown-menu-lg"><strong>У вас {{ tasks.length }} заданий</strong></b-dropdown-header>
+          <b-dropdown-item v-for="(item, id) in tasks.slice(0, 5)" :key="id">
+            <div class="small mb-1">{{ item.title }} <span class="float-right"><strong>{{ item.progress }}%</strong></span></div>
+            <b-progress height={} class="progress-xs" :variant="item.color" :value="item.progress"></b-progress>
+          </b-dropdown-item>
+          <b-dropdown-item class="text-center"><strong>Посмотреть все задания</strong></b-dropdown-item>
+        </template>
       </b-nav-item-dropdown>
 
       <b-nav-item-dropdown right no-caret>
         <template slot="button-content">
           <i class="icon-envelope-letter"></i>
-          <b-badge pill variant="info">7</b-badge>
+          <b-badge pill variant="info" v-if="messages.length > 0">{{ messages.length }}</b-badge>
         </template>
-        <div class="dropdown-menu-lg">
-        <b-dropdown-header tag="div" class="text-center dropdown-menu-lg"><strong>You have 7 messages</strong></b-dropdown-header>
+        <div class="dropdown-menu-lg" v-if="messages.length > 0">
+          <b-dropdown-header tag="div" class="text-center dropdown-menu-lg"><strong>У вас {{ messages.length }} сообщений</strong></b-dropdown-header>
 
-        <b-dropdown-item>
-          <div class="message">
+          <b-dropdown-item v-for="(item, id) in messages.slice(0, 4)" :key="id">
+            <div class="message">
             <div class="py-3 mr-3 float-left">
               <div class="avatar">
-                <img src="static/img/avatars/7.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
-                <span class="avatar-status badge-success"></span>
+                <img :src="'static/img/avatars/' + item.avatar" class="img-avatar" :alt="item.email">
+                <span :class="'avatar-status badge-' + item.variant"></span>
               </div>
             </div>
             <div>
-              <small class="text-muted">John Doe</small>
-              <small class="text-muted float-right mt-1">Just now</small>
+              <small class="text-muted">{{ item.name }}</small>
+              <small class="text-muted float-right mt-1">{{ item.registered }}</small>
             </div>
-            <div class="text-truncate font-weight-bold"><span class="fa fa-exclamation text-danger"></span> Important message</div>
-            <div class="small text-muted text-truncate">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt...</div>
-          </div>
-        </b-dropdown-item>
+            <div class="text-truncate font-weight-bold" v-html="item.title"></div>
+            <div class="small text-muted text-truncate" v-html="item.text"></div>
+            </div>
+          </b-dropdown-item>
 
-        <b-dropdown-item>
-          <div class="message">
-            <div class="py-3 mr-3 float-left">
-              <div class="avatar">
-                <img src="static/img/avatars/6.jpg" alt="admin@bootstrapmaster.com" class="img-avatar">
-                <span class="avatar-status badge-success"></span>
-              </div>
-            </div>
-            <div>
-              <small class="text-muted">John Doe</small>
-              <small class="text-muted float-right mt-1">Just now</small>
-            </div>
-            <div class="text-truncate font-weight-bold">Important message</div>
-            <div class="small text-muted text-truncate">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt...</div>
-          </div>
-        </b-dropdown-item>
-
-        <b-dropdown-item>
-          <div class="message">
-            <div class="py-3 mr-3 float-left">
-              <div class="avatar">
-                <img src="static/img/avatars/5.jpg" alt="admin@bootstrapmaster.com" class="img-avatar">
-                <span class="avatar-status badge-danger"></span>
-              </div>
-            </div>
-            <div>
-              <small class="text-muted">Janet Doe</small>
-              <small class="text-muted float-right mt-1">1:52 PM</small>
-            </div>
-            <div class="text-truncate font-weight-bold">Lorem ipsum dolor sit amet</div>
-            <div class="small text-muted text-truncate">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt...</div>
-          </div>
-        </b-dropdown-item>
-
-        <b-dropdown-item>
-          <div class="message">
-            <div class="py-3 mr-3 float-left">
-              <div class="avatar">
-                <img src="static/img/avatars/4.jpg" alt="admin@bootstrapmaster.com" class="img-avatar">
-                <span class="avatar-status badge-info"></span>
-              </div>
-            </div>
-            <div>
-              <small class="text-muted">Joe Doe</small>
-              <small class="text-muted float-right mt-1">4:03 AM</small>
-            </div>
-            <div class="text-truncate font-weight-bold">Lorem ipsum dolor sit amet</div>
-            <div class="small text-muted text-truncate">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt...</div>
-          </div>
-        </b-dropdown-item>
-
-        <a role="menuitem" href="#" target="_self" class="dropdown-item text-center"><strong>View all messages</strong></a>
-
-        <b-dropdown-item class="text-center"><strong>View all messages</strong></b-dropdown-item>
+          <b-dropdown-item class="text-center"><strong>Посмотреть все сообщения</strong></b-dropdown-item>
         </div>
       </b-nav-item-dropdown>
 
@@ -172,6 +101,55 @@ export default {
   components: {
     HeaderDropdown
   },
+  data: () => {
+    return {
+      mainMenu: [
+        { title: 'Начать', url: '/start' },
+        { title: 'Панель управления', url: '/dashboard' },
+        { title: 'Пользователи', url: '#' },
+        { title: 'Настройки', url: '#' }
+      ],
+      notifications: [
+        { icon: 'icon-user-follow text-success', text: 'Новый пользователь' },
+        { icon: 'icon-user-unfollow text-danger', text: 'Пользователь удален' },
+        { icon: 'icon-chart text-info', text: 'Отчет о продажам готов' },
+        { icon: 'icon-basket-loaded text-primary', text: 'Новый клиент' },
+        { icon: 'icon-speedometer text-warning', text: 'Сервер перегружен' }
+      ],
+      server: {
+        cpu: { value: 1, max: 4, processes: 348 },
+        memory: { value: 11444, max: 16384 },
+        ssd: { value: 243, max: 256 }
+      },
+      tasks: [
+        { title: 'Обновить NPM & Bower', progress: 0, color: 'info' },
+        { title: 'Версия ReactJS', progress:25, color: 'danger' },
+        { title: 'Версия VueJS', progress: 50, color: 'warning' },
+        { title: 'Добавить новую верстку', progress: 75, color: 'info' },
+        { title: 'Версия Angular 2 Cli', progress: 100, color: 'success' },
+        { title: 'Обновить NPM & Bower', progress: 0, color: 'info' },
+        { title: 'Версия ReactJS', progress:25, color: 'danger' },
+        { title: 'Версия VueJS', progress: 50, color: 'warning' },
+        { title: 'Добавить новую верстку', progress: 75, color: 'info' },
+        { title: 'Версия Angular 2 Cli', progress: 100, color: 'success' },
+        { title: 'Обновить NPM & Bower', progress: 0, color: 'info' },
+        { title: 'Версия ReactJS', progress:25, color: 'danger' },
+        { title: 'Версия VueJS', progress: 50, color: 'warning' },
+        { title: 'Добавить новую верстку', progress: 75, color: 'info' },
+        { title: 'Версия Angular 2 Cli', progress: 100, color: 'success' }
+      ],
+      messages: [
+        { avatar: '7.jpg', email: 'admin@bootstrapmaster.com', variant: 'success', name: 'John Doe', registered: 'Только что', title: '<span class="fa fa-exclamation text-danger"></span> Важное сообщение', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt...' },
+        { avatar: '6.jpg', email: 'admin@bootstrapmaster.com', variant: 'success', name: 'John Doe', registered: 'Только что', title: 'Важное сообщение', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt...' },
+        { avatar: '5.jpg', email: 'admin@bootstrapmaster.com', variant: 'danger', name: 'Janet Doe', registered: '13:52', title: 'Lorem ipsum dolor sit amet', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt...' },
+        { avatar: '4.jpg', email: 'admin@bootstrapmaster.com', variant: 'info', name: 'Joe Doe', registered: '04:03', title: 'Lorem ipsum dolor sit amet', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt...' },
+        { avatar: '7.jpg', email: 'admin@bootstrapmaster.com', variant: 'success', name: 'John Doe', registered: 'Только что', title: '<span class="fa fa-exclamation text-danger"></span> Важное сообщение', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt...' },
+        { avatar: '6.jpg', email: 'admin@bootstrapmaster.com', variant: 'success', name: 'John Doe', registered: 'Только что', title: 'Важное сообщение', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt...' },
+        { avatar: '5.jpg', email: 'admin@bootstrapmaster.com', variant: 'danger', name: 'Janet Doe', registered: '13:52', title: 'Lorem ipsum dolor sit amet', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt...' },
+        { avatar: '4.jpg', email: 'admin@bootstrapmaster.com', variant: 'info', name: 'Joe Doe', registered: '04:03', title: 'Lorem ipsum dolor sit amet', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt...' }
+      ] 
+    }
+  }, 
   methods: {
     sidebarToggle (e) {
       e.preventDefault()
