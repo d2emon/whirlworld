0062292e30287c7fa3f90c508657a31dc5f5d641
Dmitry Kutsenko <d2emonium@gmail.com>
2018-02-09 15:37:22 +0300

    Use bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import Vuetify from 'vuetify'
+import BootstrapVue from 'bootstrap-vue'
 import App from './App'
 import router from './router'
 
@@ -13,6 +14,7 @@ Vue.config.productionTip = false
 
 Vue.use(Vuex)
 Vue.use(Vuetify)
+Vue.use(BootstrapVue)
 
 /* eslint-disable no-new */
 new Vue({
